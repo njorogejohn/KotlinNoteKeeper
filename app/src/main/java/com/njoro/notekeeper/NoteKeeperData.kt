2c77b10ada6fb9f48debab758fd9ff186412f4aa
njorojohn <johnnjoroge40@gmail.com>
2021-07-28 01:18:54 +0300

    allow the noteinfo to accept null values

--- app/src/main/java/com/njoro/notekeeper/NoteKeeperData.kt
+++ app/src/main/java/com/njoro/notekeeper/NoteKeeperData.kt
@@ -6,4 +6,4 @@ data class CourseInfo(val courseId: String, val title: String) {
     }
 }
 
-data class NoteInfo(var course: CourseInfo, var title: String, var body: String)
+data class NoteInfo(var course: CourseInfo? = null, var title: String? = null, var body: String? = null)
